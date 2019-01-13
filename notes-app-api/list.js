8683378db6761b65c5d37e85f448587548e4d2ea
Behrooz Tabesh <btabesh@zobristinc.com>
2019-01-13 12:25:00 -0800

    add api server and react app

---
+++ notes-app-api/list.js
@@ -0,0 +1,19 @@
+import * as dynamoDBLib from './libs/dynamodb-lib'
+import { success, failure } from './libs/response-lib'
+
+export async function main(event, context) {
+  const params = {
+    TableName: 'notes',
+    KeyConditionExpression: "userId = :userId",
+    ExpressionAttributeValues: {
+      ":userId": event.requestContext.identity.cognitoIdentityId
+    }
+  }
+
+  try {
+    const result = await dynamoDBLib.call('query', params)
+    return success(result.Items)
+  } catch(e) {
+    return failure({ status: false })
+  }
+}

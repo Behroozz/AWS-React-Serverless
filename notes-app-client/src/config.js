c8dfb2f7419cbb6c1af2b0b51a4ec7e0b108ce2e
Behrooz Tabesh <btabesh@zobristinc.com>
2019-01-15 20:30:28 -0800

    add sign in page

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-west-1",
+    BUCKET: "serverless-notes-behrooz-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://488gxsovm0.execute-api.us-east-1.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_H54unlvll",
+    APP_CLIENT_ID: "4v69r690677l3rtdr0ks61v565",
+    IDENTITY_POOL_ID: "us-east-1:dee46028-37bd-48fa-95f3-5127f272e896"
+  }
+};
---
+++ notes-app-client/src/containers/NotFound.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './NotFound.css';
+
+export default () => 
+  <div clasName='NotFound'>
+    <h3>Sorry, Page not found!</h3>
+  </div>

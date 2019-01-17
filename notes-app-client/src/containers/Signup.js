0cea21c9fc9792049e0a0e5436c11ebda4740a2f
Behrooz Tabesh <btabesh@zobristinc.com>
2019-01-17 10:53:12 -0800

    connenct confirmation code to AWS amplify

--- notes-app-client/src/containers/Signup.js
+++ notes-app-client/src/containers/Signup.js
@@ -7,6 +7,7 @@ import {
 } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import "./Signup.css";
+import { Auth } from 'aws-amplify';
 
 export default class Signup extends Component {
   constructor(props) {
@@ -42,18 +43,39 @@ export default class Signup extends Component {
 
   handleSubmit = async event => {
     event.preventDefault();
-
+  
     this.setState({ isLoading: true });
-
-    this.setState({ newUser: "test" });
-
+  
+    try {
+      const newUser = await Auth.signUp({
+        username: this.state.email,
+        password: this.state.password
+      });
+      this.setState({
+        newUser
+      });
+    } catch (e) {
+      alert(e.message);
+    }
+  
     this.setState({ isLoading: false });
   }
-
+  
   handleConfirmationSubmit = async event => {
     event.preventDefault();
-
+  
     this.setState({ isLoading: true });
+  
+    try {
+      await Auth.confirmSignUp(this.state.email, this.state.confirmationCode);
+      await Auth.signIn(this.state.email, this.state.password);
+  
+      this.props.userHasAuthenticated(true);
+      this.props.history.push("/");
+    } catch (e) {
+      alert(e.message);
+      this.setState({ isLoading: false });
+    }
   }
 
   renderConfirmationForm() {
